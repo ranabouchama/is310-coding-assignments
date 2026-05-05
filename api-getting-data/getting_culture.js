763c9000508b5c02f173065cb9113e3c40ccee48
ranabouchama <ryb2@illinois.edu>
2026-05-05 15:09:31 -0500

    javascript file

---
+++ api-getting-data/getting_culture.js
@@ -0,0 +1,22 @@
+// API documentation reference:
+// /<author>/<Shakespeare>[..][/<lines>][,<title>][..][.<.json>]
+
+async function suggestEntity() {
+  try {
+    const params = new URLSearchParams({
+      text: 'Shakespeare',
+      TYPE: 'agent'
+    });
+
+    const response = await fetch(`https://www.europeana.eu/api/entity/suggest?${params}`);
+    const data = await response.json();
+    
+    console.log('Shakespeare', data);
+    return data;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+}
+
+// Call the function
+suggestEntity();
